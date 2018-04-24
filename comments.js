b82edaf3cc4a18a75b654d333726a59f95d0cbbe
KamilWojtczak123 <35527613+KamilWojtczak123@users.noreply.github.com>
2018-04-24 19:02:00 +0200

    Update comments.js

--- comments.js
+++ comments.js
@@ -7,5 +7,26 @@ function comments(state = [], action) {
                 votes: 0
             }
             , ...state.comments];
+        case REMOVE_COMMENT:
+            return [{
+                id: action.id
+            }]
+        
+        case EDIT_COMMENT:
+            return [{
+                id: action.id,
+                text: action.text
+            }]
+            
+        case THUMB_DOWN_COMMENT:
+            return [{
+            id: action.id
+    }]
+        
+            
+        case THUMB_UP_COMMENT:
+            return [{
+                id: action.id
+            }]
     }
 }
